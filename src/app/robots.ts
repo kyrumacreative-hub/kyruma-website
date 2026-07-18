880cb59b0a406d1305a966bed401a30f3286fee0
kyrumacreative-hub <kyrumacreative@gmail.com>
2026-07-18 18:34:35 +0200

    Configure AI crawler access

--- src/app/robots.ts
+++ src/app/robots.ts
@@ -2,10 +2,32 @@ import type { MetadataRoute } from "next";
 
 export default function robots(): MetadataRoute.Robots {
   return {
-    rules: {
-      userAgent: "*",
-      allow: "/",
-    },
+    rules: [
+      {
+        userAgent: "*",
+        allow: "/",
+      },
+      {
+        userAgent: "OAI-SearchBot",
+        allow: "/",
+      },
+      {
+        userAgent: "ChatGPT-User",
+        allow: "/",
+      },
+      {
+        userAgent: "GPTBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "ClaudeBot",
+        disallow: "/",
+      },
+      {
+        userAgent: "Google-Extended",
+        disallow: "/",
+      },
+    ],
     sitemap: "https://www.kyruma.com/sitemap.xml",
     host: "https://www.kyruma.com",
   };
